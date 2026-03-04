179fe4c541efe8386421213a7a6bcf56817cbcee
kevring092373 <venusmanagerde@gmail.com>
2026-03-04 22:33:10 +0100

    Revert: Blog-Liste wieder auf vorherigen Stand (Alt-Text-Änderung zurückgenommen)

--- components/BlogArticleList.tsx
+++ components/BlogArticleList.tsx
@@ -41,9 +41,7 @@ export function BlogArticleList({ posts }: Props) {
         ))}
       </div>
       <div className="blog-list blog-list--articles">
-        {filteredPosts.map((post) => {
-          const imageAlt = post.metaTitle ?? post.title ?? post.slug;
-          return (
+        {filteredPosts.map((post) => (
           <article key={post.slug} className="blog-card blog-card--article card">
             <a href={`/blog/${post.slug}`} className="blog-card-link">
               {post.image && (
@@ -51,14 +49,14 @@ export function BlogArticleList({ posts }: Props) {
                   {post.image.startsWith("/") ? (
                     <Image
                       src={post.image}
-                      alt={imageAlt}
+                      alt={post.title}
                       width={400}
                       height={220}
                       sizes="(max-width: 640px) 100vw, 400px"
                       className="blog-card-image"
                     />
                   ) : (
-                    <img src={post.image} alt={imageAlt} className="blog-card-image" />
+                    <img src={post.image} alt={post.title} className="blog-card-image" />
                   )}
                 </div>
               )}
@@ -74,8 +72,7 @@ export function BlogArticleList({ posts }: Props) {
               </div>
             </a>
           </article>
-          );
-        })}
+        ))}
       </div>
       {filteredPosts.length === 0 && (
         <p className="blog-articles-empty">Keine Beiträge in dieser Kategorie.</p>
