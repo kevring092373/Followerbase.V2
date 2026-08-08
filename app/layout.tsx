497967904683426fbb48d79dcda9b40f457b0976
kevring092373 <venusmanagerde@gmail.com>
2026-08-08 22:57:37 +0200

    Fix: Footer-Styles global auf allen Seiten

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import dynamic from "next/dynamic";
 import { DM_Sans, Outfit } from "next/font/google";
 import "./globals.css";
 import "./header.css";
+import "./footer.css";
 import { CartProvider } from "@/context/CartContext";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
